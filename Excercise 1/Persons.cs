c228fe30e4ae9bbec14e2bc14177bc3d9531c531
Jere Valtanen <jerevaltanen@msn.com>
2016-01-27 14:25:09 +0200

    Updated

---
+++ Excercise 1/Persons.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1
+{
+    /// <summary>
+    /// This class holds persons objects and gives a few methods...
+    /// </summary>
+    class Persons
+    {
+        /// <summary>
+        /// persons holds person objects...
+        /// </summary>
+        private List<Person> persons;
+
+
+        /// <summary>
+        /// Default constructor initializes a collection
+        /// </summary>
+        public Persons()
+        {
+            persons = new List<Person>();
+        }
+
+
+        /// <summary>
+        /// Add a person to collection
+        /// </summary>
+        /// <param name="person">Added person</param>
+        public void AddPerson(Person person)
+        {
+            persons.Add(person);
+        }
+
+
+        /// <summary>
+        /// Returns a person to caller application.
+        /// </summary>
+        /// <param name="index">Person index in collection</param>
+        /// <returns>Returned person object</returns>
+        public Person GetPerson(int index)
+        {
+            if (index < persons.Count) return persons.ElementAt(index);
+            else return null;
+        }
+
+        public Person FindPerson(string socialSecurityNumber)
+        {
+            foreach(Person person in persons)
+            {
+                if (socialSecurityNumber.Equals(person.SocialSecurityNumber))
+                {
+                    //Person with sotu found...
+                    return person;
+                }
+
+            }
+            // Person with sotu not found...
+            return null;
+        }
+
+        public void PrintCollection()
+        {
+
+            Console.WriteLine("Persons in collection: ");
+            foreach(Person person in persons)
+            {
+                Console.WriteLine(person.ToString());
+            }
+        }
+    }
+}
---
+++ Excercise 1/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create an object from Persons
+            Persons myFriends = new Persons();
+
+            // Create a few friends
+            Person person1 = new Person { Firstname = "Jussi", Lastname = "Jurkka", SocialSecurityNumber = "010101-01E6" };
+            Person person2 = new Person { Firstname = "Piitu", Lastname = "Piituli", SocialSecurityNumber = "020202-01A9" };
+            Person person3 = new Person { Firstname = "Elmu", Lastname = "Kelmu", SocialSecurityNumber = "030303-04T1" };
+
+            // add persons to collection
+            myFriends.AddPerson(person1);
+            myFriends.AddPerson(person2);
+            myFriends.AddPerson(person3);
+
+            // print collection
+            myFriends.PrintCollection();
+
+            // get one person
+            Console.WriteLine("Get one person from collection:");
+            myFriends.GetPerson(0);
+            Person person4 = myFriends.GetPerson(2);
+            if(person4 != null)
+            {
+                Console.WriteLine(person4.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Cannot find person in that position!");
+            }
+
+            // find person with sotu
+            Console.WriteLine("Find person with Social Security Number:");
+            string socialSecurityNumber = "030303-04T1";
+            Person person5 = myFriends.FindPerson(socialSecurityNumber);
+            if (person5 != null)
+            {
+                Console.WriteLine(person5.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Cannot find person in that position!");
+            }
+        }
+    }
+}
