0578f6ef052c6f505b1b01acd90126b449f6922a
SnakeRule <jerevaltanen@gmail.com>
2016-02-01 18:34:38 +0200

    updated

---
+++ Excercise 4/Names.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_4
+{
+    class Names
+    {
+        public string Firstname { get; set; }
+        public string Lastname { get; set; }
+        public char[] letters = new char[25] { 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'W', 'X', 'Y', 'Z' }; 
+
+        Random namerand = new Random();
+        public List<Names> names;
+
+        public Names()
+        {
+            names = new List<Names>();
+        }
+
+        public void Addname(Names fullname)
+        {
+            names.Add(fullname);
+        }
+
+        public void ShowNames()
+        {
+            foreach(Names fullname in names)
+            {
+                Console.WriteLine(fullname.Firstname + " " + fullname.Lastname);
+            }
+        }
+    }
+}
---
+++ Excercise 4/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Excercise_4
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+       char[] Temp_Firstname = new char[4];
+       char[] Temp_Lastname = new char[6];
+
+        Random namerand = new Random();
+            int generated;
+            Stopwatch timer = new Stopwatch();
+            Names allnames = new Names();
+            timer.Start();
+            for (int u = 0; u != 10000; u++)
+            {
+                for (int i = 0; i != 4; i++)
+                {
+                    generated = namerand.Next(0, 25);
+                    Temp_Firstname[i] = allnames.letters[generated];
+                }
+                for (int i = 0; i != 6; i++)
+                {
+                    generated = namerand.Next(0, 25);
+                    Temp_Lastname[i] = allnames.letters[generated];
+                }
+                allnames.Firstname = new string(Temp_Firstname);
+                allnames.Lastname = new string(Temp_Lastname);
+                Names fullname = new Names { Firstname = new string(Temp_Firstname), Lastname = new string(Temp_Lastname) };
+                allnames.Addname(fullname);
+            }
+            allnames.ShowNames();
+            Console.WriteLine(timer.ElapsedMilliseconds);
+            timer.Stop();
+        }
+    }
+}
